8f8d8b8a0feab59cb14165120bad8d28216b98b6
Neha-Singh-j <neha.singh.888.j@gmail.com>
2025-09-27 22:29:31 +0530

    assign2

---
+++ Home_Assignment2/Merge_Sorted_Arrays_88.java
@@ -0,0 +1,35 @@
+// leetcode :88
+import java.util.*;
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+       
+		int [] ans=new int [n+m];
+		int i=0,j=0,k=0;
+		while(i<m && j<n) {
+			if(nums1[i]<nums2[j]) {
+				ans[k]=nums1[i];
+				k++;
+				i++;
+			}else {
+				ans[k]=nums2[j];
+				k++;
+				j++;
+			}
+			
+		}
+		while(i<m) {
+			ans[k]=nums1[i];
+			i++;
+			k++;
+		}
+		while(j<n) {
+			ans[k]=nums2[j];
+			j++;
+			k++;
+		}
+		for( i=0;i<nums1.length;i++){
+            nums1[i]=ans[i];
+        }
+		
+	}
+}
---
+++ Home_Assignment2/Valid_Perfect_square.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        return check(num);
+    }
+    public static boolean check(int n){
+        int l=1;
+		int hi=n;
+		int ans=0;
+		while(l<=hi) {
+            int mid=(l+hi)/2;
+            long sq=(long)mid*mid;
+            if(sq==n) {
+                return true;
+                
+            }else if(sq>n) {
+
+                hi=mid-1;
+            }else{
+                l=mid+1;
+            }
+        }
+        return false;
+    }
+}
