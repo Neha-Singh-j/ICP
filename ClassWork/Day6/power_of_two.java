a57c8324ea22ecd93feb709bbc07e577a1e5ed18
Neha-Singh-j <neha.singh.888.j@gmail.com>
2025-10-03 23:00:50 +0530

    fix powerOfTwo

--- ClassWork/Day6/power_of_two.java
+++ ClassWork/Day6/power_of_two.java
@@ -1,5 +1,15 @@
 package ICP.ClassWork.Day6;
 
 public class power_of_two {
-    
+    public boolean isPowerOfTwo(int n) {
+         if (n <= 0) return false;  
+        return pow(n, 1);           // start with 2^0 = 1
+    }
+
+    public static boolean pow(int n, int val) {
+        if (val == n) return true;  // found power of 2
+        if (val > n) return false;  
+        return pow(n, val * 2);     // check next power of 2
+    }
+
 }
